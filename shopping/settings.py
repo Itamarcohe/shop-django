f66b78fb61711e6e1efcc515da51663480e659b9
Itamar Cohen <Itamar330@gmail.com>
2022-04-15 00:30:15 +0300

    Pushing to server

--- shopping/settings.py
+++ shopping/settings.py
@@ -82,6 +82,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3001",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
+    "https://shrouded-brushlands-10601.herokuapp.com/",
+    "http://shrouded-brushlands-10601.herokuapp.com/",
+    "https://shrouded-brushlands-10601.herokuapp.com",
+    "http://shrouded-brushlands-10601.herokuapp.com",
 ]
 
 
